1b8fdf3bc603d66b3591ebc8882a3aac308a2021
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-08-29 09:30:22 +0900

    add vertical

--- vue/src/layout/MainFooter.vue
+++ vue/src/layout/MainFooter.vue
@@ -1,22 +1,8 @@
 <template>
     <footer class="footer" :class="{ [`footer-${type}`]: type }" :data-background-color="backgroundColor">
         <div class="container">
-            <nav>
-                <ul>
-                    <li>
-                        <a href="https://iiif.dl.itc.u-tokyo.ac.jp/repo/s/genji/page/home">
-                  東京大学総合図書館所蔵『源氏物語』
-                </a>
-                    </li>
-                    <li>
-                        <a href="https://guides.lib.kyushu-u.ac.jp/c.php?g=774839&p=5560033">
-                  源氏物語 古活字版 - 貴重資料（九大コレクション）
-                </a>
-                    </li>
-                </ul>
-            </nav>
             <div class="copyright">
-                裏源氏研究チーム
+                TEI-C東アジア/日本語分科会
             </div>
         </div>
     </footer>
